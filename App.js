d302dbdf9877eae2a718bd7ac59b118857422d70
Stephen Chang <43056789+stephen8chang@users.noreply.github.com>
2020-04-15 20:31:53 -0500

    Fully functional directions between two locations

--- App.js
+++ App.js
@@ -1,91 +1,219 @@
-<<<<<<< HEAD
+/* Displays current latitude and longitude */
 /*import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import MapView from "react-native-maps";
-import * as Permissions from "expo-permissions";
 
 export default class App extends React.Component {
-  state = {
-    latitude: null,
-    longitude: null
-  }
-
-  async componentDidMount() {
-    const { status } = await Permissions.getAsync(Permissions.LOCATION)
-
-    if (status != 'granted') {
-      const response = await Permissions.askAsync(Permissions.LOCATION)
+  constructor() {
+    super();
+    this.state = {
+      ready: false,
+      where: { lat: null, lng: null },
+      error: null
     }
-    navigator.geolocation.getCurrentPosition(
-      ({coords: {latitude, longitude}}) => this.setState({ latitude, longitude}, () => console.long('State:' , this.state)),
-      (error) == console.log('Error: ', error)
-    )
   }
+  componentDidMount() {
+    let geoOptions = {
+      enableHighAccuracy: true,
+      timeOut: 20000,
+      maximumAge: 60 * 60 * 24
+    };
+    this.setState({ ready: false, error: null });
+    navigator.geolocation.getCurrentPosition(this.geoSuccess,
+      this.geoFailure,
+      geoOptions);
+  }
+  geoSuccess = (position) => {
+    console.log(position.coords.latitude);
 
+    this.setState({
+      ready: true,
+      where: { lat: position.coords.latitude, lng: position.coords.longitude }
+    })
+  }
+  geoFailure = (err) => {
+    this.setState({ error: err.message });
+  }
   render() {
-    const { latitude, longitude } = this.state
-
-    if (latitude) {
-      return (
-        <MapView
-          style = {{ flex : 1}}
-          initialRegion = {{
-            latitude,
-            longitude,
-            latitudeDelta: 0.0992,
-            longitudeDelta: 0.0421
-          }}
-          >
-        </MapView>
-      );
-    }
     return (
-      <View style= {{ flex : 1, justifyContent: 'center', alignItems: 'center' }}></View>
-    )
+      <View style={styles.container}>
+        {!this.state.ready && (
+          <Text style={styles.big}>Using Geolocation in React Native.</Text>
+        )}
+        {this.state.error && (
+          <Text style={styles.big}>{this.state.error}</Text>
+        )}
+        {this.state.ready && (
+          <Text style={styles.big}>{
+            `Latitude: ${this.state.where.lat}
+                    Longitude: ${this.state.where.lng}`
+          }</Text>
+        )}
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFFFFF',
+    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  big: {
+    fontSize: 48
+  }
 });
 */
 
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+/*ZEROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO*/
+/* Supposedly draws route between Bursa and Instanbul */
+/*import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions
+} from 'react-native';
 
-export default class App extends React.Component {
+import MapView from 'react-native-maps';
+import Polyline from '@mapbox/polyline';
+
+export default class RnDirectionsApp extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      coords: []
+    }
+  }
+
+  componentDidMount() {
+    // find your origin and destination point coordinates and pass it to our method.
+    // I am using Bursa,TR -> Istanbul,TR for this example
+    this.getDirections("40.1884979, 29.061018", "41.0082,28.9784")
+  }
+
+  async getDirections(startLoc, destinationLoc) {
+    try {
+      let resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc}&destination=${destinationLoc}`)
+      let respJson = await resp.json();
+      let points = Polyline.decode(respJson.routes[0].overview_polyline.points);
+      let coords = points.map((point, index) => {
+        return {
+          latitude: point[0],
+          longitude: point[1]
+        }
+      })
+      this.setState({ coords: coords })
+      return coords
+    } catch (error) {
+      alert(error)
+      return error
+    }
+  }
+
   render() {
     return (
-      <LinearGradient
-          colors={['#9232db', '#9c4fd6', '#c99aed', '#c7a3e3']}
-          style={{flex: 1}}
-          >
-            <Text style={styles.text}>Hello World</Text>
-        </LinearGradient>
+      <View>
+        <MapView style={styles.map} initialRegion={{
+          latitude: 41.0082,
+          longitude: 28.9784,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}>
+
+          <MapView.Polyline
+            coordinates={this.state.coords}
+            strokeWidth={2}
+            strokeColor="red" />
+
+        </MapView>
+      </View>
     );
   }
 }
 
-    
 const styles = StyleSheet.create({
-  text: {
-      fontSize: 56,
-      position: 'absolute',
-      left: '15%',
-      top: '40%',
-      color: 'white',
-  }
-}
-)
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
+  },
+});
 
-=======
-import React, { useState } from "react";
+AppRegistry.registerComponent('RnDirectionsApp', () => RnDirectionsApp);
+*/
+
+
+
+/* ONEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE */
+/* (Berlin) Map displays but it doesn't draw the path  */
+import React, { useState, useEffect } from "react";
+import { View, Text } from "react-native";
+import MapView, { Marker, Polyline } from "react-native-maps";
+import { decode } from "@mapbox/polyline";
+
+const getDirections = async (startLoc, destinationLoc) => {
+  try {
+    const KEY = "AIzaSyBKRbbUK2NxWb3XMqlMz69d9IYHReB9ELU";
+    let resp = await fetch(
+      `https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc}&destination=${destinationLoc}&key=${KEY}`
+    );
+    let respJson = await resp.json();
+    let points = decode(respJson.routes[0].overview_polyline.points);
+    let coords = points.map((point, index) => {
+      return {
+        latitude: point[0],
+        longitude: point[1]
+      };
+    });
+    return coords;
+  } catch (error) {
+    return error;
+  }
+};
+
+const App = () => {
+  const [coords, setCoords] = useState([]);
+
+  useEffect(() => {
+    getDirections("52.5200066,13.404954", "50.1109221,8.6821267")
+      .then(coords => setCoords(coords))
+      .catch(err => console.log("Something went wrong"));
+  }, []);
+
+  return (
+    <>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 52.5200066,
+          longitude: 13.404954,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1
+        }}
+      >
+        {coords.length > 0 && <Polyline coordinates={coords} />}
+        <Marker coordinate={{ latitude: 52.5200066, longitude: 13.404954 }} />
+        <Marker coordinate={{ latitude: 50.1109221, longitude: 8.6821267 }} />
+      </MapView>
+    </>
+  );
+};
+
+export default App;
+
+
+
+
+/* TWOOOOOOOOOOOOOOOOOOOOOOOOOOOOO */
+/* Draws a straight line from Berlin to Frankfurt */
+/*import React, { useState } from "react";
 import { View, Text } from "react-native";
 import MapView, { Marker, Polyline } from "react-native-maps";
 import { decode } from "@mapbox/polyline";
@@ -198,12 +326,15 @@ const App = () => {
 };
 
 export default App;
-
-
+*/
 /*</MapView><CustomMarker />
       </Marker> */
 // <Polyline coordinates={[Berlin, Frankfurt]} />
 
+
+
+/* THREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE */
+/* Draws tons of markers in New York Area           */
 /*
 import React from "react";
 //import App from './components/app.jsx';
@@ -273,4 +404,3 @@ export default class App extends React.Component {
 }
 
 */
->>>>>>> f30beffc423aa4b1d7957c56e9e6f73c7e54f139
